4b91efaa92a4f41247fa0f42d7a373c7ee7f23e3
Omar Qazi <omar@bqe.com>
2015-12-13 22:37:49 -0800

    added tests for update

--- controller/thread_controller.go
+++ controller/thread_controller.go
@@ -14,6 +14,10 @@ func (tc ThreadController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		tc.GetThread(rid(r), w, r)
+	case "POST":
+		tc.PostThread(w, r)
+	case "PUT":
+		tc.PutThread(w, r)
 	default:
 		tc.HandleUnknown(w, r)
 	}
@@ -34,6 +38,57 @@ func (tc ThreadController) GetThread(tid string, w http.ResponseWriter, r *http.
 		fmt.Fprintln(w, "Error marshaling response json")
 		return
 	}
+}
+
+func (tc ThreadController) PostThread(w http.ResponseWriter, r *http.Request) {
+	var thread datastore.Thread
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&thread); err != nil {
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "invalid request JSON")
+		return
+	}
+
+	if err := thread.Insert(); err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintln(w, "error saving thread")
+		return
+	}
+
+	tc.GetThread(thread.Id, w, r)
+}
+
+func (tc ThreadController) PutThread(w http.ResponseWriter, r *http.Request) {
+	var thread datastore.Thread
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&thread); err != nil {
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "invalid request JSON")
+		return
+	}
+
+	if thread.Id == "" {
+		thread.Id = rid(r)
+	}
+
+	dbThread, err := datastore.GetThread(thread.Id)
+	if err != nil {
+		w.WriteHeader(404)
+		fmt.Fprintln(w, "thread not found")
+		return
+	}
+
+	if thread.Subject == "" {
+		thread.Subject = dbThread.Subject
+	}
+
+	if err := thread.Update(); err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintln(w, "error updating thread")
+		return
+	}
+
+	tc.GetThread(thread.Id, w, r)
 }
 
 func (tc ThreadController) HandleUnknown(w http.ResponseWriter, r *http.Request) {
--- controller/thread_controller_test.go
+++ controller/thread_controller_test.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/omarqazi/hearst/datastore"
 	"net/http"
@@ -34,7 +36,140 @@ func TestThreadGetRequest(t *testing.T) {
 		return
 	}
 
+	if ct := w.Header().Get("Content-Type"); ct != "application/json" {
+		t.Error("Expected content type 'application/json' but got", ct)
+		return
+	}
+
 	if err := thread.Delete(); err != nil {
 		t.Error("Error deleting thread after test get request:", err)
+	}
+}
+
+func TestThreadPostRequest(t *testing.T) {
+	thread := datastore.Thread{
+		Subject: "I posted this from the API",
+	}
+
+	threadBytes, err := json.Marshal(thread)
+	if err != nil {
+		t.Error("Error marshaling post body JSON for thread:", err)
+		return
+	}
+
+	postBody := bytes.NewBuffer(threadBytes)
+	req, err := http.NewRequest("POST", "http://localhost:8080/thread/", postBody)
+	if err != nil {
+		t.Error("Error building POST request:", err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+	tc.ServeHTTP(w, req)
+
+	if w.Code > 299 || w.Code < 200 {
+		t.Error("Expected 200 response but got", w.Code)
+		return
+	}
+
+	if ct := w.Header().Get("Content-Type"); ct != "application/json" {
+		t.Error("Expected content type 'application/json' but got", ct)
+		return
+	}
+
+	var responseThread datastore.Thread
+	decoder := json.NewDecoder(w.Body)
+	if err := decoder.Decode(&responseThread); err != nil {
+		t.Error("Error decoding response json:", err)
+		return
+	}
+
+	if responseThread.Subject != thread.Subject {
+		t.Error("Posted subject", thread.Subject, "but got", responseThread.Subject)
+		return
+	}
+
+	trx, erx := datastore.GetThread(responseThread.Id)
+	if erx != nil {
+		t.Error("Error getting thread after POST:", erx)
+		return
+	}
+
+	if trx.Subject != thread.Subject {
+		t.Error("Posted subject", thread.Subject, "but database value was", trx.Subject)
+		return
+	}
+
+	if err := trx.Delete(); err != nil {
+		t.Error("Error deleting thread:", err)
+		return
+	}
+}
+
+func TestThreadPutRequest(t *testing.T) {
+	thread := datastore.Thread{
+		Subject: "this will be updated later",
+	}
+	if err := thread.Insert(); err != nil {
+		t.Error("Error inserting thread for put request:", err)
+		return
+	}
+
+	updatedText := "this has now been updated"
+	thread.Subject = updatedText
+
+	threadBytes, err := json.Marshal(thread)
+	if err != nil {
+		t.Error("Error marshaling PUT body JSON for thread:", err)
+		return
+	}
+
+	requestUrl := fmt.Sprintf("http://localhost:8080/thread/%s", thread.Id)
+	putBody := bytes.NewBuffer(threadBytes)
+	req, err := http.NewRequest("PUT", requestUrl, putBody)
+	if err != nil {
+		t.Error("Error building PUT request:", err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+	tc.ServeHTTP(w, req)
+
+	if w.Code > 299 || w.Code < 200 {
+		t.Error("Expected 200 response but got", w.Code)
+		return
+	}
+
+	if ct := w.Header().Get("Content-Type"); ct != "application/json" {
+		t.Error("Expected content type 'application/json' but got", ct)
+		return
+	}
+
+	var responseThread datastore.Thread
+	decoder := json.NewDecoder(w.Body)
+	if err := decoder.Decode(&responseThread); err != nil {
+		t.Error("Error decoding response json:", err)
+		return
+	}
+
+	if responseThread.Subject != thread.Subject {
+		t.Error("Posted subject", thread.Subject, "but got", responseThread.Subject)
+		return
+	}
+
+	trx, erx := datastore.GetThread(responseThread.Id)
+	if erx != nil {
+		t.Error("Error getting thread after PUT:", erx)
+		return
+	}
+
+	if trx.Subject != thread.Subject {
+		t.Error("Posted subject", thread.Subject, "but database value was", trx.Subject)
+		return
+	}
+
+	if err := trx.Delete(); err != nil {
+		t.Error("Error deleting thread:", err)
+		return
 	}
 }
