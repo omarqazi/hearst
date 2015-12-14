56d1e99bcb057980f5858ff1e234a834bc27b369
Omar Qazi <omar@bqe.com>
2015-12-14 05:19:06 -0800

    add tests for message post request

--- controller/message_controller_test.go
+++ controller/message_controller_test.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/omarqazi/hearst/datastore"
@@ -78,5 +79,69 @@ func TestMessageGetRequest(t *testing.T) {
 }
 
 func TestMessagePostRequest(t *testing.T) {
+	thread := datastore.Thread{Subject: "test message post request"}
+	if err := thread.Insert(); err != nil {
+		t.Error("Error inserting thread for test message GET request:", err)
+		return
+	}
 
+	mailbox := datastore.Mailbox{
+		PublicKey: "some-public-key",
+		DeviceId:  "some-device-id",
+	}
+	if err := mailbox.Insert(); err != nil {
+		t.Error("Error inserting mailbox for test message GET request:", err)
+		return
+	}
+
+	messageBody := "yo yo yo"
+	m := &datastore.Message{
+		ThreadId:        thread.Id,
+		SenderMailboxId: mailbox.Id,
+		Body:            messageBody,
+	}
+	m.Payload.Scan("{}")
+	m.Labels.Scan("{}")
+	messageBytes, err := json.Marshal(m)
+	if err != nil {
+		t.Error("Error marshaling message for message post request:", err)
+		return
+	}
+
+	postBody := bytes.NewBuffer(messageBytes)
+	testRequestUrl := fmt.Sprintf("http://localhost:8080/messages/%s", thread.Id)
+	req, err := http.NewRequest("POST", testRequestUrl, postBody)
+	if err != nil {
+		t.Error("Error building POST request:", err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+	mc.ServeHTTP(w, req)
+
+	if w.Code > 299 || w.Code < 200 {
+		t.Error("Expected 200 response code but got", w.Code)
+		return
+	}
+
+	messages, err := thread.RecentMessages(10)
+	if err != nil {
+		t.Error("Error getting recent messages", err)
+		return
+	}
+
+	if len(messages) != 1 {
+		t.Error("Error: expected 1 message but got", len(messages))
+		return
+	}
+
+	message := messages[0]
+
+	if message.Body != messageBody {
+		t.Error("Expected body", messageBody, "but got", message.Body)
+	}
+
+	message.Delete()
+	thread.Delete()
+	mailbox.Delete()
 }
